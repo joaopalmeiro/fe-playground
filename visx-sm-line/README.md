bf001b4ff59c03439470a4a45d7cbd6c7772502f
João Palmeiro <joaommpalmeiro@gmail.com>
2021-02-14 19:11:17 +0000

    Add data and Arquero; Update README

--- visx-sm-line/README.md
+++ visx-sm-line/README.md
@@ -9,3 +9,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## References
 
 - [Getting Started with Tachyons and React](https://github.com/tachyons-css/tachyons-and-react/tree/master/getting-started).
+- [Using the Public Folder](https://create-react-app.dev/docs/using-the-public-folder/).
+- Gaurav Singhal's "[Fetch Data from a JSON File in a React App](https://www.pluralsight.com/guides/fetch-data-from-a-json-file-in-a-react-app)" blog post.
+- [Conditionally firing an effect](https://reactjs.org/docs/hooks-reference.html#conditionally-firing-an-effect).
+- Flavio Copes' "[How to check if an object is empty in JavaScript](https://flaviocopes.com/how-to-check-object-empty/)" blog post.
--- visx-sm-line/src/App.js
+++ visx-sm-line/src/App.js
@@ -1,9 +1,45 @@
 import GrapeLayout from './GrapeLayout';
+import { useState, useEffect } from 'react';
+import * as aq from 'arquero';
 
 function App() {
+  const [table, setTable] = useState(aq.table());
+
+  const getTable = () => {
+    fetch('producao_uvas_total_regiao.csv')
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (csvString) {
+        // Fold: pivot longer
+        // Pivot: pivot wider
+
+        const rawTable = aq.fromCSV(csvString, { autoType: false });
+        const yearCols = rawTable.columnNames((col) =>
+          aq.op.startswith(col, '201')
+        );
+
+        setTable(
+          rawTable
+            .slice(0, -3)
+            .select(yearCols, { 'Total por região (milhões Kg)': 'Região' })
+            .fold(aq.not('Região'), {
+              as: ['Ano', 'Total'],
+            })
+        );
+      });
+  };
+
+  useEffect(() => {
+    getTable();
+  }, []);
+
   return (
     <div className="App ma3">
       <GrapeLayout />
+      {Object.entries(table.data()).length > 0 &&
+        table.data().constructor === Object &&
+        table.print()}
     </div>
   );
 }
